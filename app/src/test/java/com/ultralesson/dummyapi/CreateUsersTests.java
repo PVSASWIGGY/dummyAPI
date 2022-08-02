f2193606645e9e540dd526a0f8e26b6d8b5ae74f
PVSASWIGGY <venkata.penugonda@swiggy.in>
2022-08-02 16:28:27 +0530

    corrected build error by introducing countOfUsersByMe static variable

--- app/src/test/java/com/ultralesson/dummyapi/CreateUsersTests.java
+++ app/src/test/java/com/ultralesson/dummyapi/CreateUsersTests.java
@@ -4,11 +4,14 @@ import com.ultralesson.dummyapi.create.CreateUserErrorResponse;
 import com.ultralesson.dummyapi.create.CreateUserRequestBody;
 import com.ultralesson.dummyapi.create.CreateUserResponse;
 import com.ultralesson.dummyapi.users.UsersService;
+import lombok.Getter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class CreateUsersTests {
     private UsersService usersService;
+    @Getter
+    private static int countOfUsersByMe=5;
     @BeforeClass
     public void beforeClass(){
         usersService=new UsersService();
@@ -18,6 +21,7 @@ public class CreateUsersTests {
         CreateUserRequestBody body=new CreateUserRequestBody.Builder().build();
         CreateUserResponse userResponse= usersService.createUser(body);
         userResponse.assertUser(body);
+        this.countOfUsersByMe++;
     }
 
     @Test
